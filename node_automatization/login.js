10200c57ec14bf5e80a92856867ce22497a6ac9f
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2021-01-04 00:36:50 -0500

    first commit

---
+++ node_automatization/login.js
@@ -0,0 +1,46 @@
+
+
+
+
+
+
+
+
+/*
+POST /login/ HTTP/1.1
+Host: www.facebook.com
+User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+Referer: https://www.facebook.com/
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 330
+Origin: https://www.facebook.com
+Connection: close
+Cookie: fr=123LZQ8aVPeFJDeci.AWUzoaJJqqbFeh10C9NvpLxSdXA.Bf8YMO.6P.AAA.0.0.Bf8oz9.AWWPwtj80jU; sb=DoPxX9e3gacWieXay1gIPwzM; datr=DoPxX-wEhsyAtYDGcSnMs3cG; wd=950x942; locale=es_LA
+Upgrade-Insecure-Requests: 1
+
+jazoest=2957&lsd=AVpJfDk-qxA&email=ghzant.y%40gmail.com&login_source=comet_headerless_login&next=&encpass=%23PWD_BROWSER%3A5%3A1609731380%3AAYZQACdhNx16kuf826vQRe5kRlS%2FmE%2BInxRZIAEZ%2BfmTJWUnUNbJGVZIIJe%2BTTDuEnIJAzdfa0x6Srl%2Bz0xVll%2F%2BHY7wmliFiTjuzLN%2BcTkkoCH79%2FXyDqIpmn%2FqAaRje4J%2FWKEZYlocCOpd%2FEOlReKx%2F3yGjw%3D%3D
+
+
+
+# Response
+
+POST /login/ HTTP/1.1
+Host: www.facebook.com
+User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0
+Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8
+Accept-Language: en-US,en;q=0.5
+Accept-Encoding: gzip, deflate
+Referer: https://www.facebook.com/
+Content-Type: application/x-www-form-urlencoded
+Content-Length: 330
+Origin: https://www.facebook.com
+Connection: close
+Cookie: fr=123LZQ8aVPeFJDeci.AWUzoaJJqqbFeh10C9NvpLxSdXA.Bf8YMO.6P.AAA.0.0.Bf8oz9.AWWPwtj80jU; sb=DoPxX9e3gacWieXay1gIPwzM; datr=DoPxX-wEhsyAtYDGcSnMs3cG; wd=950x942; locale=es_LA
+Upgrade-Insecure-Requests: 1
+
+jazoest=2957&lsd=AVpJfDk-qxA&email=ghzant.y%40gmail.com&login_source=comet_headerless_login&next=&encpass=%23PWD_BROWSER%3A5%3A1609731380%3AAYZQACdhNx16kuf826vQRe5kRlS%2FmE%2BInxRZIAEZ%2BfmTJWUnUNbJGVZIIJe%2BTTDuEnIJAzdfa0x6Srl%2Bz0xVll%2F%2BHY7wmliFiTjuzLN%2BcTkkoCH79%2FXyDqIpmn%2FqAaRje4J%2FWKEZYlocCOpd%2FEOlReKx%2F3yGjw%3D%3D
+
+*/
