38554ccefcfd07732ac83fcc45147a94fc8f065e
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2021-01-04 19:25:31 -0500

    Request Headers

---
+++ node_automatization/requestHeaders.js
@@ -0,0 +1,28 @@
+const requestHeaders = {
+	GET:{
+		"Host": "mbasic.facebook.com",
+		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Accept": "*/*",
+		"Accept-Language": "en-US,en;q=0.5",
+		"Accept-Encoding": "gzip, deflate",
+		"Connection": "close",
+		"Content-Length":"2"
+	},
+	POST:{
+		"Host":"mbasic.facebook.com",
+		"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
+		"Accept": "*/*",
+		"Accept-Language": "en-US,en;q=0.5",
+		"Accept-Encoding": "gzip, deflate",
+		"Content-Type": "application/x-www-form-urlencoded",
+		"Content-Length": null,
+		"Origin": "https://mbasic.facebook.com",
+		"Connection": "close",
+		"Upgrade-Insecure-Requests": "1"
+	}
+}
+
+export default requestHeaders;
+
+
+
