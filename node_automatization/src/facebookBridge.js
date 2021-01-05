ffbf492253358aba889bf9ec2ad90e24f7211f46
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2021-01-05 18:38:38 -0500

    last update

--- node_automatization/src/facebookBridge.js
+++ node_automatization/src/facebookBridge.js
@@ -6,68 +6,44 @@ import tough_cookie_pkg from 'tough-cookie';
 
 const {CookieJar} = tough_cookie_pkg;
 
+
 const facebookBridge = {
 	login: async(email,pass,uid) => {
 		try{
-			const suffix = "login/device-based/regular/login/";
+			const suffix = "/login/device-based/regular/login/?refsrc=https%3A%2F%2Fmbasic.facebook.com%2F&lwv=100&refid=8t";
 
 			//Preparing cookies
 			const cookieJar = new CookieJar(),
 				cookies = cookieReader.records[uid];
 			const setCookie = promisify(cookieJar.setCookie.bind(cookieJar));
 			for(let cookie of cookies)
-				await setCookie(cookie, facebookBridge.prefixURL+suffix);
+				await setCookie(cookie, facebookBridge.prefixURL()+suffix);
 		
 			//Defining user parameters and URL-encoding them
-			let parameters = [
-				['lsd','AVpJfDk-quE'],
-				['email',email],
-				['pass',pass]
-			];
+			const parameters = new URLSearchParams({
+				'lsd':'AVpJfDk-quE',
+				'email':email,
+				'pass':pass
+			}).toString();
 
 			//Injecting cookies and including user parameters
-			parameters = new URLSearchParams(parameters);
 			let headers = requestHeaders.POST;
-			headers["Content-Length"] = parameters.toString().length; 
+			headers["Content-Length"] = parameters.length; 
 			
 			//Defining config and making request
+			axios.defaults.withCredentials = true;
 			const config = {
-				hostname:facebookBridge.hostname, 
-				method:'POST',
-				port:443,
-				path:`/${suffix}`,
+				baseURL:facebookBridge.prefixURL()+"/",
 				headers:headers,
-				data:parameters,
-			};
-			/*
-			const config = {
-				baseURL:facebookBridge.prefixURL,
-				headers:headers,
-				validateStatus: (status)=>{
-					return status>=200 && status <=399;
+				maxRedirects:0,
+				validateStatus:(status)=>{
+					return status>=200 && status <= 303;
 				},
-				maxRedirects:0
-			};*/
-			
-			//Posting
-			const instance = await axios.create(config);
-			instance.interceptors.response.use(function (response) {
-				return response
-			}, function (error) {
-				console.log({STATUS:error.response.status})
-				// Any status codes that falls outside the range of 2xx cause this function to trigger
-				// Do something with response error
-				return Promise.reject(error);
-			});
-			instance.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-			instance.post(`/${suffix}`,parameters).then((response)=>{
-				console.log(response.request.redirects);
-				console.log("\n\n=======================\n\n")
-				console.log(response);
-			}).catch((error)=>{console.log(error)});
-
-
-
+			};
+			const response = await axios.post(suffix,parameters,config);
+			cookieJar.allowSpecialUseDomain = true;
+			cookieJar.enableLooseMode = true;
+			console.log(cookieJar)	
 
 		}catch(error){
 			console.log(error)
@@ -88,6 +64,6 @@ const facebookBridge = {
 	bodyParameters:["lsd","email","pass"]
 }
 facebookBridge.get("100044781015699").then(()=>{
-	//facebookBridge.login("ghzant.y@gmail.com","password(password)","100044781015699");
+	facebookBridge.login("ghzant.y@gmail.com","password(password)","100044781015699");
 });
 //facebookBridge.makeReaction();
