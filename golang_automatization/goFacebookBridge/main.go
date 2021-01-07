17aea3175fed96de80002464ef203478870f0a09
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2021-01-06 22:55:01 -0500

    golang project upload login method (sadly fail)

--- golang_automatization/goFacebookBridge/main.go
+++ golang_automatization/goFacebookBridge/main.go
@@ -26,21 +26,15 @@ type BridgeUser struct{
 	cookies []*http.Cookie
 }
 
-type NetData struct{
-	hostname string
-	protocol string
-	port int
-}
 
 func sense(URL_string string, bridgeUser *BridgeUser)  {
-	fmt.Println("URL:",URL_string)
 	response, err := http.Get(URL_string)
 	if err!=nil{
 		fmt.Println(err)
 	}
 	var cookies []*http.Cookie
-	for _,cookie := range response.Cookies(){
-		if includes(cookieFilter,cookie.Name) {
+	for _,cookie := range response.Cookies() {
+		if includes(cookieFilter,cookie.Name){
 			cookies = append(cookies,cookie)
 		}
 	}
@@ -69,50 +63,55 @@ func main(){
 	URL_struct = makeUrl(URL_struct.String(),"login/device-based/regular/login/")
 	fmt.Println("HOST:",URL_struct.Host)
 	
-	//Adding cookies to URL 
+	//Adding cookies to URL
 	jar, _ := cookiejar.New(nil)
 	jar.SetCookies(URL_struct, bridgeUser.cookies)
-	fmt.Println(jar)
-	
+
 	// Setting parameters and econding them
 	parameters := url.Values{}
 	parameters.Set("email", bridgeUser.email)
 	parameters.Set("pass", bridgeUser.pass)
 	parameters.Set("lsd", "AVpJfDk-quE")
 	fmt.Println("Encoded Parameters:",parameters.Encode())
-	fmt.Println("URL:",URL_struct.String())
-
+	
 	// Making an HTTP Client and a New Request 
 	client :=  &http.Client{
-			Jar:jar,
 			CheckRedirect: func(request *http.Request, via []*http.Request) error {
 				fmt.Println("REDIRECT:",request,via)
 				return http.ErrUseLastResponse
-			}}
+			},
+			Jar:jar,
+		}
+	request,_:= http.NewRequest(http.MethodPost,URL_struct.String(),strings.NewReader(parameters.Encode()))
+	
+	//Adding cookies
+	for _,cookie := range bridgeUser.cookies{
+		request.AddCookie(cookie)
+	}
+	fmt.Println("Cookies:",request.Cookies())
 
-	request,_ := http.NewRequest(http.MethodPost,URL_struct.String(),strings.NewReader(parameters.Encode()))
 	//Adding Headers to Request
-	request.Header.Add("Host",URL_struct.Host)
-	request.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0")
-	request.Header.Add("Accept", "*/*")
-	request.Header.Add("Accept-Language", "en-US,en;q=0.5")
-	request.Header.Add("Accept-Encoding", "gzip, deflate")
-	request.Header.Add( "Content-Type", "application/x-www-form-urlencoded")
-	request.Header.Add( "Content-Length", strconv.Itoa(len(parameters.Encode())))
-	request.Header.Add( "Origin", URL_struct.String())
-	request.Header.Add( "Connection", "close")
-	request.Header.Add( "Upgrade-Insecure-Requests", "1")
-
+	request.Header.Set("Host",URL_struct.Host)
+	request.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	request.Header.Set("Accept", "*/*")
+	request.Header.Set("Accept-Language", "en-US,en;q=0.5")
+	request.Header.Set("Accept-Encoding", "gzip, deflate")
+	request.Header.Set( "Content-Type", "application/x-www-form-urlencoded; param=value")
+	request.Header.Set( "Content-Length", strconv.Itoa(len(parameters.Encode())))
+	request.Header.Set( "Origin", URL_struct.String())
+	request.Header.Set( "Connection", "close")
+	request.Header.Set( "Upgrade-Insecure-Requests", "1")
+	
 	//Making POST request
+	fmt.Println("POST to:",URL_struct.String())
 	response,err := client.Do(request)
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("ERROR",err)
 	}
-
+	fmt.Println("CookieJar:",jar.Cookies(request.URL))
 	fmt.Println("StatusCode:", response.StatusCode)
-	fmt.Println(response.Request.URL)
-	fmt.Println(response.Cookies())
-
+	fmt.Println("Response Cookies",response.Cookies())
+	defer response.Body.Close()
 }
 
