9fb3a993e8177ad42c6bcb6ead4a4782a1088e63
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2021-01-09 17:31:38 -0500

    project has been restructured

--- golang_automatization/goFacebookBridge/main.go
+++ golang_automatization/goFacebookBridge/main.go
@@ -1,25 +1,18 @@
 package main
 
 import(
-	"fmt"
 	"fbreach"
-    	"net/url"
 )
 
 
 func main(){
 	//Creating UserBreach
 	userBreach := fbreach.CreateUser("ghzant.y@gmail.com","password(password)")
-	fmt.Println("User:",userBreach)
 
 	// GET for getting part of Cookies
-	URL_struct,_ := url.Parse("https://mbasic.facebook.com/")
-	userBreach.Sense(URL_struct)
-	fmt.Println("User Sensed:",userBreach)
+	userBreach.Sense()
 
-	
-		//Defining URL
-	URL_struct,_ = url.Parse(URL_struct.String()+"login/device-based/regular/login/")
-	userBreach.Rip(URL_struct)
+	// POST for get cookie-credentials
+	userBreach.Rip()
 }
 
