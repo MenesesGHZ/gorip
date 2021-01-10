dfd15d10f2b59ae70796bacf28ce98ad871e8ebd
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2021-01-09 21:36:53 -0500

    added action for get name

--- golang_automatization/goFacebookBridge/main.go
+++ golang_automatization/goFacebookBridge/main.go
@@ -7,12 +7,25 @@ import(
 
 func main(){
 	//Creating UserBreach
-	userBreach := fbreach.CreateUser("ghzant.y@gmail.com","password(password)")
+	userBreach := fbreach.CreateUser("gerry_csm@outlook.com","print(jerry2000)")
 
 	// GET for getting part of Cookies
 	userBreach.Sense()
 
 	// POST for get cookie-credentials
 	userBreach.Rip()
+	
+	// Prepare 
+	config := fbreach.ActionConfig{
+		GetBasicInfo:true,
+		MakeReaction:false,
+		MakePost:false,
+	}
+	content := fbreach.ActionContent{}
+	
+	// DO
+	userBreach.Do(content,config)
+
+
 }
 
