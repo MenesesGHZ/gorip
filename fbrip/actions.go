972d6a936e95a9c7fcc7ad3ab30792125a10d6a3
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2021-01-11 05:59:34 -0500

    working on makeReaction() action

--- fbrip/actions.go
+++ fbrip/actions.go
@@ -6,47 +6,63 @@ import(
 	"golang.org/x/net/html"
 )
 
-type ActionConfig struct {
+type ActionConfig struct{
 	GetBasicInfo bool
-	MakeReaction bool
-	MakePost bool
+	React react
+	Post post
+	Comment comment
 }
 
-type ActionContent struct {
-	url *url.URL
-	reaction int
-	comment string
-}
-
-//
 // ACTIONS
-//
-func(u *UserBreach) getBasicInfo() bool{
-
+func(u *UserRip) getBasicInfo(){
 	// Making GET request
 	URL_struct,_ := url.Parse("https://mbasic.facebook.com/profile.php?v=info")
 	response := u.GET(URL_struct)
 
-	// Just getting the name (for now)
+	//TEMPORAL IMPLEMENTATION. Just getting the name (for now)
 	z := html.NewTokenizer(response.Body)
 	for {
 		tt := z.Next()
 		if tt == html.ErrorToken {
-			// ...
-			return false
+			break
 		}
 		if string(z.Raw()) == "<title>"{
 			tt = z.Next()
-			fmt.Println("> Welcome ->",z.Token())	
-			u.name = string(z.Raw())
+
+			// TEMPORAL SOLUTION. It needs to be determine from the rip's phases whether the user could login or not.
+			out := string(z.Raw())
+			if out == "Page Not Found" || out == "Pagina No Encontrada"{
+				panic("> FAILED AT LOGIN ;(")
+				break
+			}
+			fmt.Println("> Welcome ->",out)
+			u.Info.Name = out
+			break
 		}
 	}
-	return true
 }
 
-func(u *UserBreach) makeReaction(url *url.URL, reaction int) bool{
-	success:=false
-	return success
+func(u *UserRip) makeReaction(URL *url.URL, reaction string){
+	//Getting Query Parameters from `URL`	
+	values := URL.Query()
+	//Setting Query Parameters
+	URL_struct,_ := URL.Parse("https://mbasic.facebook.com/ufi/reaction/")
+	rValues := url.Values{}
+	if URL.Path == "photo.php"{
+		//Missing -> av, ext, hash
+		rValues.Set("reaction_type", reaction)
+		rValues.Set("basic_origin_uri",URL.String())
+		rValues.Set("_ft_",values.Get("_ft_"))
+		rValues.Set("ft_ent_identifier",values.Get("set")[3:])// to delete the extra `gm.`
+	}
+	//Adding Query Parameters to `URL_struct`
+	URL_struct.RawQuery = rValues.Encode()
+	fmt.Println("RawQuery",URL_struct.RawQuery)
+	//Making GET request
+	u.GET(URL_struct)	
 }
 
 
+//func makeReactionPhase1(){
+//	//
+//}
