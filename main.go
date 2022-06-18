fc617b3cefa3351ed7fb527f4d770548d6e8adf0
Gerardo Hernández <52936839+MenesesGHZ@users.noreply.github.com>
2022-06-17 20:50:09 -0700

    Update main.go

--- main.go
+++ main.go
@@ -5,5 +5,5 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello :) This is Gorip. Work in progress...")
+	fmt.Println("Hello :) This is Gorip. Feel free to checkout the `examples/` folder for more usage examples.")
 }
